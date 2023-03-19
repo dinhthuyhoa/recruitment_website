725e35d7a02c4ba569916e98d3832d0ba554d3f5
P Lê <90311759+phamle21@users.noreply.github.com>
2023-03-19 20:42:15 +0700

    build source

---
+++ app/Http/Controllers/PostFavoriteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PostFavorite;
+use Illuminate\Http\Request;
+
+class PostFavoriteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PostFavorite  $postFavorite
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PostFavorite $postFavorite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PostFavorite  $postFavorite
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PostFavorite $postFavorite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PostFavorite  $postFavorite
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PostFavorite $postFavorite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PostFavorite  $postFavorite
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PostFavorite $postFavorite)
+    {
+        //
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable()->unique();
+            $table->string('gender')->nullable();
+            $table->string('birthday')->nullable();
+            $table->text('avatar')->nullable();
+            $table->string('email')->unique();
+            $table->string('status')->default('Active');;
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('role');
+            $table->rememberToken();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
